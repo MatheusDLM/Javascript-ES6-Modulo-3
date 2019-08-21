a51d1bb6176e55788c49240c167d81f437b0ca30
Matheus Maia <matheus.maia100@hotmail.com>
2019-08-21 02:32:59 -0300

    Modulo 3

--- scr/main.js
+++ scr/main.js
@@ -1,29 +1,58 @@
-import ClasseUsuario from './function';
+const delay = () => new Promise(resolve => setTimeout(resolve, 1000));
 
-// 1.1
-
-ClasseUsuario.info();
-
-// 1.2
-
-import {idade} from './function';
-
-console.log(idade);
-
-// 1.3
-
-import Usuario, {idade as IdadeUsuario} from './function';
-import { resolve } from 'path';
-
-// Teste
-const minhaPromise = () => new Promise((resolve,reject) => {
-    setTimeout(() => { resolve('OK') },2000);
-});
-
-async function executaPromise() {
-    const response = await minhaPromise();
-
-    console.log(response)
+async function umPorSegundo() {
+await delay();
+console.log("1s");
+await delay();
+console.log("2s");
+await delay();
+console.log("3s");
 }
 
-executaPromise();
+umPorSegundo();
+
+// 2
+
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(username) {
+        try{
+        const response = await axios.get(`https://api.github.com/users/${username}`);
+        console.log(response.data);
+    } catch (err) {
+            console.warn('Usuário não existe');
+        }
+    }
+}
+
+Api.getUserInfo('MatheusDLM');
+
+// 3
+
+class Github {
+    static async getRepositories(repo) {
+        try {
+            const response = await axios.get(`https://api.github.com/repos/${repo}`);
+            console.log(response.data);
+        } catch (err) {
+            console.log('Repositório não existe');
+        };
+    }
+}
+Github.getRepositories('MatheusDLM/Javascript-ES6');
+Github.getRepositories('rocketseat/dslkvmskv');
+
+// 4
+
+const buscaUsuario = async usuario => {
+    try {
+      const response = await axios.get(`https://api.github.com/users/${usuario}`);
+  
+      console.log(response.data);
+    } catch (err) {
+      console.log("Usuário não existe");
+    }
+  };
+  
+  buscaUsuario("MatheusDLM");
